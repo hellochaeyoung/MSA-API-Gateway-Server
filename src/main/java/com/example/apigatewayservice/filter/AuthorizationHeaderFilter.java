d1c875451415139a3171b60ee36bb3e7917f1e8d
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-09-29 21:13:21 +0900

    [chore] JWT 인증 관련 설명 주석 추가

--- src/main/java/com/example/apigatewayservice/filter/AuthorizationHeaderFilter.java
+++ src/main/java/com/example/apigatewayservice/filter/AuthorizationHeaderFilter.java
@@ -50,7 +50,7 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
         String subject = null;
 
         try {
-            subject = Jwts.parser().setSigningKey(env.getProperty("token.secret"))
+            subject = Jwts.parser().setSigningKey(env.getProperty("token.secret")) // JWS 서명 검증을 위한 secretkey 혹은 비대칭 공개키를 지정
                 .parseClaimsJws(jwt).getBody()
                 .getSubject();
         } catch (Exception e) {
